6bea175e662055791dda64ff47c0e662b70bc1bc
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-29 21:42:40 +0530

    section-added

--- script.js
+++
@@ -1,9 +0,0 @@
-const mobile_nav = document.querySelector(".mobile-navbar-btn");
-const nav_header = document.querySelector(".headerone");
-
-const toggleNavbar = () => {
-  nav_header.classList.toggle("active");
-};
-
-mobile_nav.addEventListener("click", () => toggleNavbar());
-
