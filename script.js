b73223cbcfce6831d1aead29effa890c095454af
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-31 05:15:23 +0530

    footer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Handle Form Submission
-const form = document.querySelector(".contact-us__form");
+const form = document.querySelector(".contact-us___form");
 const errorAlert = document.querySelector(".contact-us__form-alert");
 const alertClose = document.querySelector(".contact-us__form-alert-close");
 
@@ -29,3 +29,15 @@ form.addEventListener("submit", (e) => {
 alertClose.addEventListener("click", () => {
   errorAlert.style.display = "none";
 });
+
+
+
+  // const scriptURL = 'https://script.google.com/macros/s/AKfycbyDFWJp-hR2ACpOzJ2MuOqiRNIRD2AbI8NArFhTcvGP3uwIYdPEeMEpfhx14_sMH6dO/exec'
+  // const form = document.forms['Contact-us']
+
+  // form.addEventListener('submit', e => {
+  //   e.preventDefault()
+  //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  //     .then(response => console.log('Success!', response))
+  //     .catch(error => console.error('Error!', error.message))
+  // })
