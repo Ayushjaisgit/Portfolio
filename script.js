7a605d8d3ab7c90ae6d9fd56b5ef08cf92aa270c
Ayushjaisgit <ayushcodez@gmail.com>
2022-10-31 11:31:21 +0530

    Resume added

--- script.js
+++ script.js
@@ -32,12 +32,3 @@ alertClose.addEventListener("click", () => {
 
 
 
-  // const scriptURL = 'https://script.google.com/macros/s/AKfycbyDFWJp-hR2ACpOzJ2MuOqiRNIRD2AbI8NArFhTcvGP3uwIYdPEeMEpfhx14_sMH6dO/exec'
-  // const form = document.forms['Contact-us']
-
-  // form.addEventListener('submit', e => {
-  //   e.preventDefault()
-  //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  //     .then(response => console.log('Success!', response))
-  //     .catch(error => console.error('Error!', error.message))
-  // })
