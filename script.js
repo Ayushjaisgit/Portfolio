7bc6759ccb119d1ddb0906a1b1be7334e4b9e025
Ayush Jaiswal <109768481+Ayushjaisgit@users.noreply.github.com>
2023-09-01 16:17:33 +0530

    updated experience

--- script.js
+++ script.js
@@ -30,5 +30,14 @@ alertClose.addEventListener("click", () => {
   errorAlert.style.display = "none";
 });
 
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+    e.preventDefault();
+  }
+});
 
 
